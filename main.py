a9ab3069ef72cffb29e716dfb1222c987c79772e
banzdec <it.avng@gmail.com>
2026-03-20 22:04:54 +0700

    study: create a bare textual app

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from daily-tui!")
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
 
+class StopWatchApp(App):
+    """Textual app for a stop watch"""
+    
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """"create child widget for the app"""
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """Action to switch to dark mode"""
+
+        self.theme = ( "textual-dark" if self.theme == "textual-light" else "textual-light")
 
 if __name__ == "__main__":
-    main()
+    app = StopWatchApp()
+
+    app.run()
