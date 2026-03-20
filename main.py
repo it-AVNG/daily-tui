8b47c038d24c19a9ea16aeaee8cb59c618cf66d1
banzdec <it.avng@gmail.com>
2026-03-20 22:58:00 +0700

    study: add widget method to TimeDisplay to display the elapse time

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, Button, Digits
 from textual.containers import HorizontalGroup, VerticalScroll
-
+from textual.reactive import reactive
+from time import monotonic
 
 class TimeDisplay(Digits):
     """AWidget to display the time"""
 
-    pass
+    start_time = reactive(monotonic)
+    time = reactive(0.0)
+
+    def update_time(self) -> None:
+        """method to update time to the current time"""
+        self.time = monotonic() - self.start_time
+
+    def on_mount(self) -> None:
+        """event handler call when widget is added to the class"""
+        self.set_interval(1 / 60, self.update_time)
+
+    def watch_time(self, time:float)-> None:
+        minutes, seconds = divmod(time, 60)
+        hours, minutes = divmod(minutes, 60)
+        self.update(f"{hours:02,.0f}:{minutes:02.0f}:{seconds:05.2f}")
+
 
 
 class StopWatch(HorizontalGroup):
@@ -26,7 +42,7 @@ class StopWatch(HorizontalGroup):
         yield Button("Stop", id="stop", variant="error")
         yield Button("Reset", id="reset")
 
-        yield TimeDisplay("00:00:00:00")
+        yield TimeDisplay()
 
 
 class StopWatchApp(App):
