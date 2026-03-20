8e48cacb15c6e1b7d3a29eb6e9c3c2d49fc498bb
banzdec <it.avng@gmail.com>
2026-03-20 22:12:32 +0700

    study: create A widget app called stop watch, add them to the main app

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, Button, Digits
+from textual.containers import HorizontalGroup, VerticalScroll
+
+
+class TimeDisplay(Digits):
+    """AWidget to display the time"""
+    pass
+
+
+class StopWatch(HorizontalGroup):
+    """A stop watch widget"""
+    def compose(self) -> ComposeResult:
+        """Create a child widget for stop watch"""
+        yield Button("Start", id="start", variant="success")
+        yield Button("Stop", id="stop", variant="error")
+        yield Button("Reset", id="reset")
+
+        yield TimeDisplay("00:00:00:00")
+
+
 
 class StopWatchApp(App):
     """Textual app for a stop watch"""
@@ -10,6 +29,7 @@ class StopWatchApp(App):
         """"create child widget for the app"""
         yield Header()
         yield Footer()
+        yield VerticalScroll(StopWatch(), StopWatch(), StopWatch())
 
     def action_toggle_dark(self) -> None:
         """Action to switch to dark mode"""
